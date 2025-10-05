0dc08fab569116a99aaf03dd0cf69cdf979d52a3
shaojie <shaojie483@gmail.com>
2025-10-05 02:14:12 +0200

    aaa

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,4 @@ pub use info::{info_client::*, *};
 pub use market_maker::{MarketMaker, MarketMakerInput, MarketMakerRestingOrder};
 pub use meta::{AssetContext, AssetMeta, Meta, MetaAndAssetCtxs, SpotAssetMeta, SpotMeta};
 pub use ws::*;
+pub use signature::{sign_l1_action, sign_typed_data};
--- src/signature/create_signature.rs
+++ src/signature/create_signature.rs
@@ -5,7 +5,7 @@ use alloy::{
 
 use crate::{eip712::Eip712, prelude::*, signature::agent::l1, Error};
 
-pub(crate) fn sign_l1_action(
+pub fn sign_l1_action(
     wallet: &PrivateKeySigner,
     connection_id: B256,
     is_mainnet: bool,
@@ -18,7 +18,7 @@ pub(crate) fn sign_l1_action(
     sign_typed_data(&payload, wallet)
 }
 
-pub(crate) fn sign_typed_data<T: Eip712>(
+pub fn sign_typed_data<T: Eip712>(
     payload: &T,
     wallet: &PrivateKeySigner,
 ) -> Result<Signature> {
